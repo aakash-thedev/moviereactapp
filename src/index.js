35e8e17c488bf32e519ff8f5914b6380d56f1c12
Aakash <aakashsrivastava008@gmail.com>
2020-08-14 00:49:49 +0530

    store added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers/reducer';
+
+const store = createStore(movies);
+console.log('State : ', store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
