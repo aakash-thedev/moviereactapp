c7e64ef263454ca7c3d4251a76c461ca0b79d590
Aakash <aakashsrivastava008@gmail.com>
2020-08-14 18:49:19 +0530

    added dispatch fucntion to add actions on the reducers to the store

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
-function App() {
-  return (
-    <div className = "App">
-      <Navbar />
-      <div className = "main">
+class App extends React.Component{
 
-        <div className = "tabs">
-          <div className = "tab">Movies</div>
-          <div className = "tab">Favourites</div>
-        </div>
+  componentDidMount = () => {
+    
+  }
 
-        <div className = 'list'>
-          { 
-            data.map(movie => (
-              <MovieCard movieData = {movie} />
-            ))
-          }
+  render(){
+
+    const movies = this.props.store.getState();
+    return (
+      <div className = "App">
+        <Navbar />
+        <div className = "main">
+
+          <div className = "tabs">
+            <div className = "tab">Movies</div>
+            <div className = "tab">Favourites</div>
+          </div>
+
+          <div className = 'list'>
+            { 
+              movies.map((movie, index) => (
+                <MovieCard movieData = {movie}
+                key = {`movies ${index}`}
+                />
+              ))
+            }
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
